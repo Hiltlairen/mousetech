f687d5919ec83f2a9459849b364f4e9787bbfacc
William <sirjhoswilliam5@gmail.com>
2025-04-30 10:59:54 -0400

    nose

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar"
+import Footer from "./Components/Footer"
 import Home from "./pages/Home";
 import Servicios from "./pages/Servicios";
 import Proyectos from "./pages/Proyectos";
